c823f9f9f2381cb1b69d778e8f1c1d7dd854d097
Vu Tran <tranhoaivu02091995@gmail.com>
2020-03-14 21:50:01 +0700

    abc

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,36 +6,36 @@ import * as wijmo from 'wijmo/wijmo';
 import { CustomGridEditor } from './app.customEditor';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-  private abc = null;
-  private products: any = (data as any).default;
-  private nameList: any = [];
-  ngOnInit() {
-    this.products.forEach(item => {
-        item.date = this.convertDate(item.date);
-    });
-  }
-  initializeGrid(flex: wjcGrid.FlexGrid) {
-    new CustomGridEditor(flex, 'date', wjcInput.InputDate, {
-      format: 'd'
-    });
-    flex.hostElement.addEventListener('click', function(e) {
-			var hti = flex.hitTest(e);
-			if(hti.cellType == wjcGrid.CellType.Cell) {
-				flex.startEditing(true, hti.row, hti.col);
-			}
-		})
-  };
-  convertDate (str) {
-    let yy = str.substring(0,4);
-    let mm = str.substring(4,6);
-    let dd = str.substring(6,8);
-    return new Date(yy,mm,dd);
-  }
+    constructor() { }
+    private abc = null;
+    private products: any = (data as any).default;
+    private nameList: any = [];
+    ngOnInit() {
+        this.products.forEach(item => {
+            item.date = this.convertDate(item.date);
+        });
+    }
+    initializeGrid(flex: wjcGrid.FlexGrid) {
+        new CustomGridEditor(flex, 'date', wjcInput.InputDate, {
+            format: 'd'
+        });
+        flex.hostElement.addEventListener('click', function (e) {
+            var hti = flex.hitTest(e);
+            if (hti.cellType == wjcGrid.CellType.Cell) {
+                flex.startEditing(true, hti.row, hti.col);
+            }
+        })
+    };
+    convertDate(str) {
+        let yy = str.substring(0, 4);
+        let mm = str.substring(4, 6);
+        let dd = str.substring(6, 8);
+        return new Date(yy, mm, dd);
+    }
 }
